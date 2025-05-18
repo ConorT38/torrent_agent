0f92033dd95902810aa70bfd98e8aa8769b24ada
Conor Thompson <conorthompson213@gmail.com>
2025-05-18 10:39:34 +0100

    add try catch to not fail entire run

--- torrent_agent/torrent_agent.py
+++ torrent_agent/torrent_agent.py
@@ -22,13 +22,17 @@ async def main():
         file_name = Path(file_path).stem
         log.info("Processing file: " + file_name)
 
-        extension = "." + file_path.split(".")[-1].lower()
-        if extension in NON_BROWSER_FRIENDLY_VIDEO_FILETYPES or extension in BROWSER_FRIENDLY_VIDEO_FILETYPES:
-            await process_video(file_name, file_path)
-        elif extension in IMAGE_FILETYPES:
-           await process_image(file_name, file_path)
-        else:
-            log.info(f"File '{file_name}' is not a supported format. Skipping.")
+        try:
+            extension = "." + file_path.split(".")[-1].lower()
+            if extension in NON_BROWSER_FRIENDLY_VIDEO_FILETYPES or extension in BROWSER_FRIENDLY_VIDEO_FILETYPES:
+                await process_video(file_name, file_path)
+            elif extension in IMAGE_FILETYPES:
+                await process_image(file_name, file_path)
+            else:
+                log.info(f"File '{file_name}' is not a supported format. Skipping.")
+                continue
+        except Exception as e:
+            log.error(f"An error occurred while processing file '{file_name}': {e}")
             continue
         
 if __name__ == "__main__":
