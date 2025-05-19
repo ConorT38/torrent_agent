5e0425609b5332f465f67ee6f5e6ac5bf77581a4
Conor Thompson <conorthompson213@gmail.com>
2025-05-19 08:56:25 +0100

    updating async conversion

--- torrent_agent/video/video_processor.py
+++ torrent_agent/video/video_processor.py
@@ -37,7 +37,7 @@ async def insert_file_metadata(filename, file_uuid):
     
     entertainment_type = str(file_uuid.split("/")[3])  # Extract the entertainment type from the path
     log.info("Inserting " + filename + " into the database. "+entertainment_type)
-    log.debug(f"Insert values: file_uuid={file_uuid}, cdn_path={file_uuid.replace('/media', '')}, title={filename}, entertainment_type={entertainment_type}")
+    log.debug(f"Insert values: file_uuid={file_uuid}, cdn_path={file_uuid.replace('/mnt/ext1', '')}, title={filename}, entertainment_type={entertainment_type}")
     try:
         await connection.insert(
             f"INSERT INTO videos (filename, cdn_path, title, uploaded, entertainment_type) VALUES ('{file_uuid}', '{file_uuid.replace('/media', '')}', '{filename}', NOW(), '{entertainment_type}')"
