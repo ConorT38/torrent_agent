34b5eb0bc2df1ea71c03ca41b7c2b87fa52fcef8
Conor Thompson <conorthompson213@gmail.com>
2025-06-16 12:05:58 +0100

    fix image repo bug

--- torrent_agent/database/images_repository.py
+++ torrent_agent/database/images_repository.py
@@ -10,8 +10,8 @@ class ImagesRepository(IImagesDAO):
         self.db = db
 
     async def get_image(self, image_id) -> 'Image':
-        log.info(f"Retrieving video with ID: {image_id}")
-        result = await self.db.query("SELECT * FROM images WHERE filename = ?", [image_id])
+        log.info(f"Retrieving image with ID: {image_id}")
+        result = await self.db.query(f"SELECT * FROM images WHERE filename = '{image_id}'")
         if result:
             video_data = result[0]
             return Image(
@@ -22,7 +22,7 @@ class ImagesRepository(IImagesDAO):
         return None
 
     async def add_image(self, image: 'Image') -> int:
-        log.info(f"Inserting {image.file_name} into the database.")
+        log.info(f"Inserting image {image.file_name} into the database.")
         try:
             last_row_id = await self.db.insert(
                 f"INSERT INTO images (filename, cdn_path, uploaded) VALUES ('{image.file_name}', '{image.cdn_path}', NOW())")
