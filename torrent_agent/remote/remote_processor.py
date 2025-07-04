f8215d19212364864d214c125aad96a55cf95abd
Conor Thompson <conorthompson213@gmail.com>
2025-07-04 12:14:11 +0100

    updating remote dir for shows and movies

--- torrent_agent/remote/remote_processor.py
+++ torrent_agent/remote/remote_processor.py
@@ -47,25 +47,6 @@ class RemoteProcessor:
             log.error(f"Error checking file on remote host {host}: {e}")
             return False
 
-    def _scp_file_to_remote(self, host, local_path, remote_path):
-        """
-        SCP a file to the remote host.
-        :param host: Remote host IP.
-        :param local_path: Path to the local file.
-        :param remote_path: Path to copy the file to on the remote host.
-        """
-        try:
-            log.debug(f"Copying file to remote host {host}: {local_path} -> {remote_path}")
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(host, username=self.get_username(host))
-            with SCPClient(ssh.get_transport()) as scp:
-                scp.put(local_path, remote_path)
-            log.debug(f"File {local_path} copied to {host}:{remote_path}")
-            ssh.close()
-        except Exception as e:
-            log.error(f"Error copying file to remote host {host}: {e}")
-
     def _get_next_host(self):
         """
         Get the next host in a round-robin fashion.
@@ -102,10 +83,38 @@ class RemoteProcessor:
         except Exception as e:
             log.error(f"Error checking free space on remote host {host}: {e}")
             return False
+        
+    def _scp_file_to_remote(self, host, local_path, remote_path):
+        """
+        SCP a file to the remote host, creating necessary directories if they don't exist.
+        :param host: Remote host IP.
+        :param local_path: Path to the local file.
+        :param remote_path: Path to copy the file to on the remote host.
+        """
+        try:
+            log.debug(f"Copying file to remote host {host}: {local_path} -> {remote_path}")
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(host, username=self.get_username(host))
+            
+            # Create the directory structure on the remote host
+            remote_dir = os.path.dirname(remote_path)
+            mkdir_cmd = f"mkdir -p {remote_dir}"
+            ssh.exec_command(mkdir_cmd)
+            log.debug(f"Ensured directory exists on remote host {host}: {remote_dir}")
+            
+            # Copy the file
+            with SCPClient(ssh.get_transport()) as scp:
+                scp.put(local_path, remote_path)
+            log.debug(f"File {local_path} copied to {host}:{remote_path}")
+            ssh.close()
+        except Exception as e:
+            log.error(f"Error copying file to remote host {host}: {e}")
 
     def process_file(self, local_path):
         """
         Process a file by SCPing it to a remote host in a round-robin fashion and removing it locally.
+        If the file is being sent back to the control host, ensure it is placed in the same folder structure.
         :param local_path: Path to the local file.
         """
         if not os.path.exists(local_path):
@@ -125,7 +134,8 @@ class RemoteProcessor:
             host = self.configuration.get_control_agent_host()
         else:
             host = self._get_next_host()
-            remote_path = f"/home/{self.get_username(host)}/conversions/{os.path.basename(local_path)}"
+            relative_path = os.path.relpath(local_path, "/mnt/ext1/torrents")
+            remote_path = f"/home/{self.get_username(host)}/conversions/{relative_path}"
 
         if not self._has_enough_space_on_remote(host, remote_path):
             log.error(f"Not enough space on remote host {host} for file {local_path}. Skipping.")
